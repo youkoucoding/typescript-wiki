c37caf64d772106db8c8e8d9ede57939b046ebcb
youkoucoding <youkousai@gmail.com>
2021-04-24 15:58:38 +0900

    Add react-with-ts

---
+++ react-with-ts/README.md
@@ -0,0 +1,5 @@
+#### Getting Started with Create React App
+
+---
+
+`yarn create react-app react-with-ts --template typescript`
---
+++ react-with-ts/src/components/Hello.tsx
@@ -0,0 +1,13 @@
+interface IHelloProps {
+  message: string;
+}
+
+const Hello = (props:IHelloProps) => {
+  return (
+    <h2>
+      {props.message}
+    </h2>
+  )
+}
+
+export default Hello;
