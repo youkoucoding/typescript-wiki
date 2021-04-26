ae65f67ff496cdebc158496c07e905c2d7453aae
youkoucoding <youkousai@gmail.com>
2021-04-26 16:25:30 +0900

    Optimize Button

--- aries/src/App.tsx
+++ aries/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Button, { ButtonSize, ButtonType } from './components/Button/button';
 
 function App() {
@@ -7,11 +6,28 @@ function App() {
       <header className='App-header'>
         <Button disabled> hey how are you!</Button>{' '}
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
-          hey how are you!
+          Large Primary
         </Button>
         <Button btnType={ButtonType.Link} href='https://www.google.com'>
-          {' '}
           hey how are you!
+        </Button>
+        <Button btnType={ButtonType.Default} href='https://www.google.com'>
+          hey how are you!
+        </Button>
+        <Button btnType={ButtonType.Danger} href='https://www.google.com'>
+          hey how are you!
+        </Button>
+        <Button btnType={ButtonType.Primary} href='https://www.google.com'>
+          hey
+        </Button>
+        <Button autoFocus>AutoFocus</Button>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            alert('test pass!');
+          }}
+        >
+          OnClick
         </Button>
       </header>
     </div>
--- aries/src/components/Button/button.tsx
+++ aries/src/components/Button/button.tsx
@@ -21,12 +21,29 @@ interface BaseButtonProps {
   children: React.ReactNode;
   href?: string;
 }
+// 此处使用 intersection Types  交叉类型 用&连接而不是 ｜  使得新对象安全的拥有两者的全部属性
+//TypeScript 提供了为类型注解设置别名的便捷语法，你可以使用 type SomeName = someValidTypeAnnotation
+type NativeButtonProps = BaseButtonProps &
+  React.ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps &
+  React.ButtonHTMLAttributes<HTMLElement>;
 
-const Button: React.FC<BaseButtonProps> = (props) => {
-  const { btnType, size, disabled, children, href } = props;
+// 可使用 Parial<T> 将属性都设置为可选属性，这样容易实现接口中的属性，或者不实现。
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const {
+    btnType,
+    className,
+    size,
+    disabled,
+    children,
+    href,
+    ...restProps
+  } = props;
 
   // btn btn-lg btn-secondary
-  const classes = classNames('btn', {
+  const classes = classNames('btn', className, {
     [`btn-${btnType}`]: btnType, // 对象key 是可变的时候，可采用 此写法，中括号加 模版字符串
     [`btn-${size}`]: size,
     disabled: btnType === ButtonType.Link && disabled,
@@ -34,13 +51,13 @@ const Button: React.FC<BaseButtonProps> = (props) => {
 
   if (btnType === ButtonType.Link && href) {
     return (
-      <a className={classes} href={href}>
+      <a className={classes} href={href} {...restProps}>
         {children}
       </a>
     );
   } else {
     return (
-      <button className={classes} disabled={disabled}>
+      <button className={classes} disabled={disabled} {...restProps}>
         {children}
       </button>
     );
